c449a5624cc840be41e6e833f864e09ba25aa0ea
Alec1999 <a.ours1999@gmail.com>
2025-03-06 12:05:29 -0800

    Write the logic to get the computer choice
    Created function that returns either "rock",
    "paper", or "scissors" randomly, with equal chance
    for any of the three.

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("Hello world!")
+const log = console.log;
+
+function getComputerChoice() {
+
+    let randNumber = Math.random()
+
+    // Make variable equal to either rock paper or scissors depending on number
+    if (randNumber <= 0.33)
+        outcome = "rock"
+    else if (randNumber > 0.33 && randNumber <= 0.66)
+        outcome = "paper"
+    else
+        outcome = "scissors"
+
+    log(outcome)
+    return outcome;
+}
+
+getComputerChoice();
