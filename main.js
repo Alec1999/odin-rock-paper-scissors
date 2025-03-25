8ca69b55465ff12293421523a7a60fb1f5eae37c
Alec1999 <a.ours1999@gmail.com>
2025-03-24 19:15:42 -0700

    Add logic to determine overall winner of game
    Added the functions to determine who won the five
    rounds, as well as display messages for each
    round. Also updated exsisting messages, and made
    the game look nicer in the console.

--- main.js
+++ main.js
@@ -19,19 +19,22 @@ function getComputerChoice() {
 function getHumanChoice() {
 
     // collect human choice for rock, paper, or scissors
-    let humanChoice = prompt("What is your choice?")
+    let humanChoice = prompt("Rock, paper or scissors?")
     return humanChoice;
 }
 
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
+    let roundNum = 1;
     
     for (let i = 0; i < 5; i++) {
         computerChoice = getComputerChoice()
         humanChoice = getHumanChoice()
 
-        winner = playRound(humanChoice, computerChoice, humanScore, computerScore)
+        winner = playRound(humanChoice, computerChoice, humanScore, computerScore, roundNum)
+
+        roundNum++
 
         if (winner == "human")
             humanScore++
@@ -39,55 +42,69 @@ function playGame() {
             computerScore++
     }
 
-    function playRound(humanChoice, computerChoice, humanScore, computerScore) {
+    log(" ")
+
+    if (humanScore == computerScore)
+        log("You tied with the computer!")
+    else if(humanScore > computerScore)
+        log("Congratulations! You beat the computer " + humanScore + " to " + computerScore)
+    else
+        log("You lost " + humanScore + " to " + computerScore + " against the computer. Try again?")
+
+
+    function playRound(humanChoice, computerChoice, humanScore, computerScore, roundNumber) {
 
         let humanChoiceLower = humanChoice.toLowerCase();
         let winner = ""
     
-        log("computer choice is now " + computerChoice)
-        log("human choice is now " + humanChoiceLower)
+        log(" ")
+        log("The computer chose " + computerChoice)
+        log("You chose " + humanChoiceLower)
+        log(" ")
     
         // If player chooses rock:
         if (humanChoiceLower == "rock")
             if (computerChoice == "rock")
-                log("You tied!")
+                log("You tied round " + roundNumber + "!")
             else if (computerChoice == "scissors")
-                log("You win! Rock beats scissors"),
+                log("You win in round " + roundNumber + "! Rock beats scissors"),
                 winner = "human",
                 humanScore++
             else
-                log("You lose! Paper beats rock"),
+                log("You lose round " + roundNumber + "! Paper beats rock"),
                 winner = "computer",
                 computerScore++
     
         // If player chooses scissors:
         else if (humanChoiceLower == "scissors")
             if (computerChoice == "scissors")
-                log("You tied!")
+                log("You tied in round " + roundNumber + "!")
             else if (computerChoice == "paper")
-                log("You win! scissors beats paper"),
+                log("You win round " + roundNumber + "! scissors beats paper"),
                 winner = "human",
                 humanScore++
             else
-                log("You lose! rock beats scissors"),
+                log("You lose round " + roundNumber + "! rock beats scissors"),
                 winner = "computer",
                 computerScore++
     
         // If player chooses paper
         else   
             if (computerChoice == "paper")
-                log("You tied!")
+                log("You tied in round " + roundNumber + "!")
             else if (computerChoice == "rock")
-                log("You win! paper beats rock"),
+                log("You win round " + roundNumber + "! paper beats rock"),
                 winner = "human",
                 humanScore++
             else
-                log("You lose! scissors beats paper"),
+                log("You lose round " + roundNumber + "! scissors beats paper"),
                 winner = "computer",
                 computerScore++
         
-        log("Human score is " + humanScore)
-        log("Computer score is " + computerScore)
+        log(" ")
+        log("Your score is " + humanScore)
+        log("The computers score is " + computerScore)
+        log(" ")
         log("--------------------------------------")
 
         return winner;
